0bc2bac1f306e5b3b0d13d3c024a3354e430a456
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-17 09:51:14 +0200

    wip

--- lib/helpers/link_back.rb
+++ lib/helpers/link_back.rb
@@ -13,8 +13,36 @@ end
 
 def link_back
   parent = find_parent(item.identifier.to_s)
-  if parent
-    '<p>↑ ' + link_to("Back to #{parent[:short_title] || parent[:title]}", parent) + '</p>'
+  siblings =
+    if parent
+      @items
+        .find_all(parent.identifier.without_ext + '/*')
+        .sort_by { |i| i[:title] }
+    else
+      []
+    end
+
+  index = siblings.find_index { |i| i == @item }
+
+  next_item = index && index < siblings.size ? siblings[index+1] : nil
+  prev_item = index && index > 0 ? siblings[index-1] : nil
+
+  if parent || next_item || prev_item
+    s = []
+
+    if parent
+      s << ['↑ ' + link_to("Back to #{parent[:short_title] || parent[:title]}", parent)]
+    end
+
+    if prev_item
+      s << ['&larr; ' + link_to("Previous (#{prev_item[:short_title] || prev_item[:title]})", prev_item)]
+    end
+
+    if next_item
+      s << ['&rarr; ' + link_to("Next (#{next_item[:short_title] || next_item[:title]})", next_item)]
+    end
+
+    '<p>' + s.join('<br>') + '</p>'
   else
     ''
   end
